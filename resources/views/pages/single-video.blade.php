7a9f4333428c8b1705db5d257b7b16af1648761b
Sigit <sigit.idn@gmail.com>
2021-10-30 07:53:32 +0900

    Fix single page

--- resources/views/pages/single-video.blade.php
+++ resources/views/pages/single-video.blade.php
@@ -28,6 +28,12 @@ function replaceTag(element){
                         <div class="col-lg-12">
                             <div class="gen-video-holder">
                                 <?php
+                                    $video["video_url"] = str_replace("ifrome", "iframe", $video["video_url"]);
+                                    $video["video_url_2"] = str_replace("ifrome", "iframe", $video["video_url_2"]);
+                                    $video["video_url_3"] = str_replace("ifrome", "iframe", $video["video_url_3"]);
+                                    $video["video_url_4"] = str_replace("ifrome", "iframe", $video["video_url_4"]);
+                                    $video["video_url_5"] = str_replace("ifrome", "iframe", $video["video_url_5"]);
+
                                     $youtubeVideoID = [];
                                     preg_match("/(\w|-|_){11}/", $video["video_url"], $youtubeVideoID);
 
